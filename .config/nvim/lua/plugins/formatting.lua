e13509fc3452eecca50940c85a83b206f2b6e362
Haffi Mazhar <haffi@move.ai>
2024-04-20 04:39:28 +0100

    lazy vim config update

---
+++ .config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                python = { "isort", "black" },
+                lua = { "stylua" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                javascript = { "prettier" },
+                javascriptreact = { "prettier" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            },
+        })
+
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            })
+        end)
+    end,
+}
